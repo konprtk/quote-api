56297f7892e3ceaf1555fc257d5266bfbb4205ee
Pratik Lokhande <pratik127001@gmail.com>
2025-01-22 16:12:28 +0530

    first commit

---
+++ quoteAPI.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const quotes = process.env.QUOTES;
+const PORT = process.env.PORT || 3000;
+
+async function fetchAndDisplayQuote() {
+  try {
+    const response = await fetch(quotes);
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+    let data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+  }
+}
+
+// Endpoint to get a random quote
+app.get('/quote', async (req, res) => {
+  try {
+    const quoteData = await fetchAndDisplayQuote();
+    if (!quoteData) throw new Error('No data received!');
+    res.json(quoteData);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'Failed to fetch the quote. Please try again later.' });
+  }
+});
+
+
+app.listen(PORT, () =>
+  console.log(`Quotes API running on http://localhost:${PORT}`)
+);
